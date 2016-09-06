f95a44b01d1bece1f3edeec9b04a743abf5d4c3d
Qiang Ji <qiang.ji@yahoo.com.au>
2016-09-06 16:24:17 +1000

    Add map.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
-ReactDOM.render(
-  <p>Hello, world!</p>,
-  document.getElementById('app')
-);
+var map,
+    mapEl = document.getElementById('vic-emergency-map'),
+    mapWidth = mapEl.offsetWidth;
+
+function initMap() {
+  mapEl.style.height = mapWidth / 1.618 + "px";
+  map = new google.maps.Map(mapEl, {
+    center: {lat: -36.4251149, lng: 145.0603821},
+    zoom: 7,
+    mapTypeControl: false,
+    streetViewControl: false
+  });
+}
+
+window.addEventListener('resize', function() {
+  mapWidth = mapEl.offsetWidth;
+  mapEl.style.height = mapWidth / 1.618 + "px";
+})
