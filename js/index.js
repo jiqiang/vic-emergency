67092192350bcbbfaa88d2adbf8c3452a09f0fab
Qiang Ji <qiang.ji@yahoo.com.au>
2016-09-06 16:49:40 +1000

    Add markers.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var map,
     mapEl = document.getElementById('vic-emergency-map'),
-    mapWidth = mapEl.offsetWidth;
+    mapWidth = mapEl.offsetWidth,
+    markers = [];
 
 function initMap() {
   mapEl.style.height = mapWidth / 1.618 + "px";
@@ -12,7 +13,32 @@ function initMap() {
   });
 }
 
+function dropMakers(map, emergency, index) {
+
+  setTimeout(function() {
+    var marker = new google.maps.Marker({
+      map: map,
+      animation: google.maps.Animation.DROP,
+      position: {lat: emergency.latitude, lng: emergency.longitude}
+    });
+    markers.push(marker);
+  }, index * 200);
+
+}
+
 window.addEventListener('resize', function() {
   mapWidth = mapEl.offsetWidth;
   mapEl.style.height = mapWidth / 1.618 + "px";
 })
+
+$.ajax({
+  url: 'https://data.emergency.vic.gov.au/Show?pageId=getIncidentJSON',
+  method: 'GET',
+  cache: false,
+  dataType: 'json'
+})
+.done(function(response) {
+  emergencies = response.results.map(function(item, index) {
+    dropMakers(map, item, index);
+  });
+});
