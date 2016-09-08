b088141e7c929e83513653804cecdc853f55c254
Qiang Ji <qiang.ji@yahoo.com.au>
2016-09-08 16:55:41 +1000

    Use openlayers library.

--- js/index.js
+++ js/index.js
@@ -1,35 +1,79 @@
 var map,
-    mapEl = document.getElementById('vic-emergency-map'),
-    mapWidth = mapEl.offsetWidth,
-    markers = [];
+    baseLayer,
+    emergencyLayer,
+    source,
+    view,
+    feature,
+    polygon,
+    size,
+    style,
+    emergencyFeature;
 
-function initMap() {
-  mapEl.style.height = mapWidth / 1.618 + "px";
-  map = new google.maps.Map(mapEl, {
-    center: {lat: -36.4251149, lng: 145.0603821},
-    zoom: 7,
-    mapTypeControl: false,
-    streetViewControl: false
-  });
-}
+style = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(255, 255, 255, 0)'
+  }),
+  stroke: new ol.style.Stroke({
+    color: 'rgba(255, 255, 255, 0)'
+  }),
+  image: new ol.style.Circle({
+    radius: 5,
+    fill: new ol.style.Fill({
+      color: 'rgba(255, 0, 0, 1)'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#319FD3',
+      width: 1
+    })
+  })
+});
 
-function dropMakers(map, emergency, index) {
+source = new ol.source.Vector({
+  url: '/data/vic.json',
+  format: new ol.format.GeoJSON()
+});
 
-  setTimeout(function() {
-    var marker = new google.maps.Marker({
-      map: map,
-      animation: google.maps.Animation.DROP,
-      position: {lat: emergency.latitude, lng: emergency.longitude}
-    });
-    markers.push(marker);
-  }, index * 200);
+baseLayer = new ol.layer.Tile({
+  source: new ol.source.OSM()
+});
 
-}
+emergencyLayer = new ol.layer.Vector({
+  source: source,
+  style: style
+});
+
+view = new ol.View({
+  center: [0, 0],
+  zoom: 1
+});
+
+map = new ol.Map({
+  target: 'vic-emergency-map',
+  layers: [baseLayer, emergencyLayer],
+  view: view,
+  controls: ol.control.defaults({
+    attributionOptions: {
+      collapsible: false
+    }
+  })
+});
+
+size = map.getSize();
+
+source.once('addfeature', function(e) {
+  polygon = e.feature.getGeometry()
+  view.fit(polygon, size, {constrainResolution: false});
+});
 
 window.addEventListener('resize', function() {
-  mapWidth = mapEl.offsetWidth;
-  mapEl.style.height = mapWidth / 1.618 + "px";
+
 })
+
+function getEmergencyFeature(emergency) {
+  return new ol.Feature({
+    geometry: new ol.geom.Point(ol.proj.transform([emergency.longitude, emergency.latitude], 'EPSG:4326', 'EPSG:3857'))
+  });
+}
 
 $.ajax({
   url: 'https://data.emergency.vic.gov.au/Show?pageId=getIncidentJSON',
@@ -38,7 +82,8 @@ $.ajax({
   dataType: 'json'
 })
 .done(function(response) {
-  emergencies = response.results.map(function(item, index) {
-    dropMakers(map, item, index);
+  var features = response.results.map(function(item) {
+    return getEmergencyFeature(item);
   });
+  source.addFeatures(features);
 });
